134a8be4a664b115948be20691498ce5af7ffe0d
KaneAya <115447877+KaneAya@users.noreply.github.com>
2022-10-24 15:30:58 +0900

    Update main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -11,3 +11,23 @@ let StoryText="It was 94 fahrenheit outside, so :insertx: went for a walk. When 
 
 let insertX=["Willy the Goblin","Big Daddy","Father Christmas"];
 let insertY=["the soup kitchen","Disneyland","the White House"];
+let insertZ=["spontaneously combusted","melted into a puddle on the sidewalk","turned into a slug and crawled away"];
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+  if(customName.value !== '') {
+    const name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
