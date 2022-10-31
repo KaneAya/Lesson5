44fd560f3467779546a7a7c446ff6d86c5ce3663
KaneAya <115447877+KaneAya@users.noreply.github.com>
2022-10-31 15:01:08 +0900

    Update main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -17,7 +17,7 @@ randomize.addEventListener('click', result);
 
 function result() {
 
-  let newStory=storyText;
+  let newStory=StoryText;
   
   let xItem=randomValueFromArray(insertX);
   let yItem=randomValueFromArray(insertY);
