b39ceb0e218751630aad31eac31e14263c4abf3e
KaneAya <115447877+KaneAya@users.noreply.github.com>
2022-10-31 15:03:14 +0900

    Update main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -34,8 +34,8 @@ function result() {
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+    const weight = Math.round(300*0.0714286);
+    const temperature =  Math.round((94-32)/1.8);
 
   }
 
